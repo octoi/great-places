c4550a8716ebfedf9f0b1fa6ca15f2021f70c9f4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-18 16:16:50 +0530

    adding location package setup

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({Key? key}) : super(key: key);
+
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration:
+              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+          child: _previewImageUrl == ''
+              ? Text('No location chosen', textAlign: TextAlign.center)
+              : Image.network(
+                  _previewImageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlatButton.icon(
+              icon: Icon(Icons.location_on),
+              label: Text('Current Location'),
+              textColor: Theme.of(context).primaryColor,
+              onPressed: () {},
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.map),
+              label: Text('Select On Map'),
+              textColor: Theme.of(context).primaryColor,
+              onPressed: () {},
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
